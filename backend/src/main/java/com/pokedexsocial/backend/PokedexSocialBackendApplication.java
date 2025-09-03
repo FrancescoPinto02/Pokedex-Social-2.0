79e5d37f2f5523f3643a952bbc20ca981f25deae
FrancescoPinto02 <francesco.pintop@gmail.com>
2025-09-03 15:36:41 +0200

    Initial commit: setup monorepo backend/frontend

---
+++ backend/src/main/java/com/pokedexsocial/backend/PokedexSocialBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pokedexsocial.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexSocialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexSocialBackendApplication.class, args);
+	}
+
+}
